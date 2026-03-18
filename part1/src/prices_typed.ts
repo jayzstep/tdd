6b34d286689aefe6d4c0c583bcdd2f484f134546
Jasse <jasse.merivirta@helsinki.fi>
2026-03-18 09:31:31 +0200

    r

--- part1/src/prices_typed.ts
+++ part1/src/prices_typed.ts
@@ -19,8 +19,8 @@ function createApp(database: Database) {
     const age = req.query.age ? parseInt(req.query.age as string) : undefined;
     const type = req.query.type as string;
     const baseCost = database.findBasePriceByType(type)!.cost;
-    const date = parseDate(req.query.date as string);
-    const cost = calculateCost(age, type, convert(date), baseCost);
+    const date = convert(parseDate(req.query.date as string));
+    const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
 
