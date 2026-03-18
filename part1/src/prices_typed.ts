15c0976efa8301a1c03950a7c1cd1b94b5aa4e9b
Jasse <jasse.merivirta@helsinki.fi>
2026-03-18 09:28:50 +0200

    r

--- part1/src/prices_typed.ts
+++ part1/src/prices_typed.ts
@@ -80,7 +80,7 @@ function createApp(database: Database) {
     return Math.ceil(baseCost * (1 - reduction / 100));
   }
 
-  function calculateReduction(date: Date | undefined) {
+  function calculateReduction(date: Temporal.PlainDate | undefined) {
     let reduction = 0;
     if (date && isMonday(date) && !isHoliday(date)) {
       reduction = 35;
