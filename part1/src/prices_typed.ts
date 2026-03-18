55e2b2d8f5f47cb0d2c14bd7ea8ddae116913e22
Jasse <jasse.merivirta@helsinki.fi>
2026-03-18 09:18:26 +0200

    r

--- part1/src/prices_typed.ts
+++ part1/src/prices_typed.ts
@@ -103,6 +103,7 @@ function createApp(database: Database) {
       let holiday2 = Temporal.PlainDate.from(row.holiday);
       if (
         date &&
+        convert(date).equals(holiday2) &&
         date.getFullYear() === holiday.getFullYear() &&
         date.getMonth() === holiday.getMonth() &&
         date.getDate() === holiday.getDate()
