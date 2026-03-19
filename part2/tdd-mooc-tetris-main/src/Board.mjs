1bc5cda580a57c6cb342cf569962fc409fb33ba9
Jasse <jasse.merivirta@helsinki.fi>
2026-03-19 20:57:11 +0200

    r

--- part2/tdd-mooc-tetris-main/src/Board.mjs
+++ part2/tdd-mooc-tetris-main/src/Board.mjs
@@ -12,6 +12,7 @@ export class Board {
     let result = "";
     const result2 = [];
     for (let i = 0; i < this.height; i++) {
+      result2.push([]);
       for (let j = 0; j < this.width; j++) {
         result += ".";
       }
