eb1f5e3c0c03be75ec83f8aae1cf04cdffd5de4d
Jasse <jasse.merivirta@helsinki.fi>
2026-03-22 16:24:03 +0200

    r

--- part2/tdd-mooc-tetris-main/src/RotatingShape.mjs
+++ part2/tdd-mooc-tetris-main/src/RotatingShape.mjs
@@ -3,6 +3,15 @@ export class RotatingShape {
     this.shape = shape;
   }
   static fromString(shape) {
+    const trimmed = shape.replace(/\s/g, "").split("");
+    const result = [];
+
+    for (let i = 0; i < 3; i++) {
+      result[i] = [];
+      for (let j = 0; j < 3; j++) {
+        result[i].push(trimmed[i * 3 + j]);
+      }
+    }
     return new RotatingShape(shape);
   }
 
