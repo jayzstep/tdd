a80565c9b2bc6c8a90b4d7995142c19e3fa2af2f
Jasse <jasse.merivirta@helsinki.fi>
2026-03-22 20:42:54 +0200

    r

--- part2/tdd-mooc-tetris-main/src/Tetromino.mjs
+++ part2/tdd-mooc-tetris-main/src/Tetromino.mjs
@@ -1,6 +1,12 @@
+import { RotatingShape } from "./RotatingShape.mjs";
+
 export class Tetromino {
+  constructor(rotatingShape) {
+    this.shape = rotatingShape;
+  }
   static get T_SHAPE() {
-    return new Tetromino();
+    const t_shape = ".T.\nTTT\n...\n";
+    return new Tetromino(RotatingShape.fromString(t_shape));
   }
 
   toString() {
