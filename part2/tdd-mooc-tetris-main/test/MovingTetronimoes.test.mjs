84649928f3bcde3a28c5e28e9f24d6183b9b20f1
Jasse <jasse.merivirta@helsinki.fi>
2026-03-28 19:27:51 +0200

    t tetromino can be moved left

---
+++ part2/tdd-mooc-tetris-main/test/MovingTetronimoes.test.mjs
@@ -0,0 +1,25 @@
+import { beforeEach, describe, test } from "vitest";
+import { expect } from "chai";
+import { Board } from "../src/Board.mjs";
+import { Tetromino } from "../src/Tetromino.mjs";
+
+describe("Moving tetrominoes", () => {
+  let board;
+  beforeEach(() => {
+    board = new Board(10, 6);
+  });
+
+  test("can be moved left", () => {
+    board.drop(Tetromino.T_SHAPE);
+    board.moveLeft();
+
+    expect(board.toString()).to.equalShape(
+      `...T......
+       ..TTT.....
+       ..........
+       ..........
+       ..........
+       ..........`
+    );
+  });
+});
