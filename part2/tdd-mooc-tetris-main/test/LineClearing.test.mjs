1b637e2ebf074699d4af388bb78225d625eccb50
Jasse <jasse.merivirta@helsinki.fi>
2026-04-11 21:38:47 +0300

    t Line clearing a single full row

---
+++ part2/tdd-mooc-tetris-main/test/LineClearing.test.mjs
@@ -0,0 +1,27 @@
+import { beforeEach, describe, test } from "vitest";
+import { expect } from "chai";
+import { Board } from "../src/Board.mjs";
+
+describe("Line clearing", () => {
+  let board;
+  beforeEach(() => {
+    board = new Board(3, 3);
+  });
+
+  test("single full row disappears", () => {
+    board.setState([
+      [".", ".", "."],
+      [".", ".", "."],
+      ["B", ".", "B"],
+    ]);
+    board.drop("B");
+    board.tick();
+    board.tick();
+
+    expect(board.toString()).to.equalShape(
+      `...
+       ...
+       ...`
+    );
+  });
+});
