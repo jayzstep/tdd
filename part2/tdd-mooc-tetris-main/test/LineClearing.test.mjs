52f482874fb9cbd3d2208ce3504987b3a108d2e5
Jasse <jasse.merivirta@helsinki.fi>
2026-04-12 18:54:00 +0300

    t + g two rows disappear

--- part2/tdd-mooc-tetris-main/test/LineClearing.test.mjs
+++ part2/tdd-mooc-tetris-main/test/LineClearing.test.mjs
@@ -1,6 +1,7 @@
 import { beforeEach, describe, test } from "vitest";
 import { expect } from "chai";
 import { Board } from "../src/Board.mjs";
+import { Tetromino } from "../src/Tetromino.mjs";
 
 describe("Line clearing", () => {
   let board;
@@ -41,6 +42,32 @@ describe("Line clearing", () => {
       `...
        ...
        B..`
+    );
+  });
+  test("two rows disappear", () => {
+    board = new Board(6, 6);
+    board.setState([
+      [".", ".", ".", ".", ".", "."],
+      [".", ".", ".", ".", ".", "."],
+      [".", ".", ".", ".", ".", "."],
+      [".", ".", ".", ".", ".", "."],
+      ["B", "B", ".", ".", "B", "B"],
+      ["B", "B", ".", ".", "B", "B"],
+    ]);
+    board.drop(Tetromino.O_SHAPE);
+    board.tick();
+    board.tick();
+    board.tick();
+    board.tick();
+    board.tick();
+
+    expect(board.toString()).to.equalShape(
+      `......
+       ......
+       ......
+       ......
+       ......
+       ......`
     );
   });
 });
