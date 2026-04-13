384b3cd957e90f72508ba93ecb76e16a62f1660c
Jasse <jasse.merivirta@helsinki.fi>
2026-04-13 22:11:59 +0300

    t scooring 2 rows gives 100 points

--- part2/tdd-mooc-tetris-main/test/Scoring.test.mjs
+++ part2/tdd-mooc-tetris-main/test/Scoring.test.mjs
@@ -7,8 +7,14 @@ describe("Scoring", () => {
   beforeEach(() => {
     scoring = new Scoring();
   });
+
   test("cleared row scores 40 points", () => {
     scoring.update(1);
     expect(scoring.score).to.equal(40);
+  });
+
+  test("two cleared rows scores 100 points", () => {
+    scoring.update(2);
+    expect(scoring.score).to.equal(100);
   });
 });
