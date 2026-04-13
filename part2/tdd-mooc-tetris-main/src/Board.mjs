5306a22b0754cbb6abe826557e923134c4f32a94
Jasse <jasse.merivirta@helsinki.fi>
2026-04-13 20:52:12 +0300

    g

--- part2/tdd-mooc-tetris-main/src/Board.mjs
+++ part2/tdd-mooc-tetris-main/src/Board.mjs
@@ -140,7 +140,7 @@ export class Board {
       ...this.state.filter((_row, index) => !fullRows.has(index)),
     ];
     if (this.subscriber && fullRows.size > 0) {
-      this.subscriber.update();
+      this.subscriber.update(1);
     }
   }
 
