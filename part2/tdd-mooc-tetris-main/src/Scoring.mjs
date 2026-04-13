7905dc2321971d49396edd42827b92238420174d
Jasse <jasse.merivirta@helsinki.fi>
2026-04-13 22:27:44 +0300

    g

--- part2/tdd-mooc-tetris-main/src/Scoring.mjs
+++ part2/tdd-mooc-tetris-main/src/Scoring.mjs
@@ -1,11 +1,21 @@
 export class Scoring {
+  constructor() {
+    this.score = 0;
+  }
   update(rowCount) {
     switch (rowCount) {
       case 1:
-        this.score = 40;
+        this.score += 40;
         break;
       case 2:
-        this.score = 100;
+        this.score += 100;
+        break;
+      case 3:
+        this.score += 300;
+        break;
+      case 4:
+        this.score += 1200;
+        break;
     }
   }
 }
