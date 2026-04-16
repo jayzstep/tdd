6bc009d755d1e41254b5744deee6774fa4cd919b
Jasse <jasse.merivirta@helsinki.fi>
2026-04-16 21:21:54 +0300

    t multiple rows updates score + g

--- part2/tdd-mooc-tetris-main/test/BoardScoring.test.mjs
+++ part2/tdd-mooc-tetris-main/test/BoardScoring.test.mjs
@@ -2,6 +2,7 @@ import { beforeEach, describe, test } from "vitest";
 import { expect } from "chai";
 import { Board } from "../src/Board.mjs";
 import { Scoring } from "../src/Scoring.mjs";
+import { Tetromino } from "../src/Tetromino.mjs";
 
 describe("Board scoring", () => {
   let board;
@@ -20,6 +21,21 @@ describe("Board scoring", () => {
     ]);
     expect(scoring.score).to.equal(0);
     board.drop("B");
+    board.tick();
+    board.tick();
+    board.tick();
+    expect(scoring.score).to.equal(40);
+  });
+
+  test("clearing multiple rows updates score", () => {
+    board.setState([
+      [".", ".", "."],
+      [".", ".", "."],
+      [".", ".", "B"],
+      [".", ".", "B"],
+    ]);
+
+    board.drop(Tetromino.O_SHAPE);
     board.tick();
     board.tick();
     board.tick();
