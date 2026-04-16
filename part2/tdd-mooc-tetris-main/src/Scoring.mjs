89326c46b9a0698ff65f4959c8e6f9a66cd7d075
Jasse <jasse.merivirta@helsinki.fi>
2026-04-16 20:26:57 +0300

    g

--- part2/tdd-mooc-tetris-main/src/Scoring.mjs
+++ part2/tdd-mooc-tetris-main/src/Scoring.mjs
@@ -6,20 +6,20 @@ export class Scoring {
   }
   update(rowCount) {
     this.clearedRows += rowCount;
-    this.level = Math.min(Math.floor(this.clearedRows / 10) + 1, 10);
     switch (rowCount) {
       case 1:
-        this.score += 40;
+        this.score += 40 * this.level;
         break;
       case 2:
-        this.score += 100;
+        this.score += 100 * this.level;
         break;
       case 3:
-        this.score += 300;
+        this.score += 300 * this.level;
         break;
       case 4:
-        this.score += 1200;
+        this.score += 1200 * this.level;
         break;
     }
+    this.level = Math.min(Math.floor(this.clearedRows / 10) + 1, 10);
   }
 }
