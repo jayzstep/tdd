974e1da6a6ba7cb275e3d00d6fded751ac72997b
Jasse <jasse.merivirta@helsinki.fi>
2026-04-19 18:21:39 +0300

    t shufflebag init

---
+++ part2/tdd-mooc-tetris-main/test/Shufflebag.test.mjs
@@ -0,0 +1,12 @@
+import { beforeEach, describe, test } from "vitest";
+import { expect } from "chai";
+import { Shufflebag } from "../src/Shufflebag.mjs";
+
+describe("Shufflebag", () => {
+  const shufflebag = new Shufflebag();
+  test("returns a value between 0 and 6", () => {
+    const values = [0, 1, 2, 3, 4, 5, 6];
+    const value = shufflebag.next();
+    expect(values).to.include(value);
+  });
+});
