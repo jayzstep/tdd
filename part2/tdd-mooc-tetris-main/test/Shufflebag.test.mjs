5cdc20783921a8b4a55ef72b750e12fa1cd2e65b
Jasse <jasse.merivirta@helsinki.fi>
2026-04-19 19:01:08 +0300

    r

--- part2/tdd-mooc-tetris-main/test/Shufflebag.test.mjs
+++ part2/tdd-mooc-tetris-main/test/Shufflebag.test.mjs
@@ -3,7 +3,10 @@ import { expect } from "chai";
 import { Shufflebag } from "../src/Shufflebag.mjs";
 
 describe("Shufflebag", () => {
-  const shufflebag = new Shufflebag();
+  let shufflebag;
+  beforeEach(() => {
+    shufflebag = new Shufflebag();
+  });
   test("returns a value between 0 and 6", () => {
     const values = [0, 1, 2, 3, 4, 5, 6];
     const value = shufflebag.next();
