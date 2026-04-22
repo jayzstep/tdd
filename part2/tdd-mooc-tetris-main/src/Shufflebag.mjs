44de2519c5e04e769181f24fef85823e986bf839
Jasse <jasse.merivirta@helsinki.fi>
2026-04-22 21:21:51 +0300

    r

--- part2/tdd-mooc-tetris-main/src/Shufflebag.mjs
+++ part2/tdd-mooc-tetris-main/src/Shufflebag.mjs
@@ -1,17 +1,17 @@
 export class Shufflebag {
   constructor() {
-    this.data2 = [0, 1, 2, 3, 4, 5, 6];
-    this.index = this.data2.length - 1;
+    this.data = [0, 1, 2, 3, 4, 5, 6];
+    this.index = this.data.length - 1;
   }
   next() {
     const randomIndex = Math.floor(Math.random() * this.index);
-    const result2 = this.data2[randomIndex];
-    this.data2[randomIndex] = this.data2[this.index];
-    this.data2[this.index] = result2;
+    const result = this.data[randomIndex];
+    this.data[randomIndex] = this.data[this.index];
+    this.data[this.index] = result;
     this.index--;
     if (this.index < 0) {
-      this.index = this.data2.length - 1;
+      this.index = this.data.length - 1;
     }
-    return result2;
+    return result;
   }
 }
