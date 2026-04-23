c2e0ddd79a37a4ea3149c54a027a592f057a41b3
Jasse <jasse.merivirta@helsinki.fi>
2026-04-23 18:38:52 +0300

    add rest of the tetrominos

--- part2/tdd-mooc-tetris-main/src/Tetromino.mjs
+++ part2/tdd-mooc-tetris-main/src/Tetromino.mjs
@@ -52,6 +52,82 @@ export class Tetromino {
     return new Tetromino([o_shape], 0);
   }
 
+  static get S_SHAPE() {
+    const s_shape1 = `....
+                      .SS.
+                      SS..
+                      ....
+`;
+    const s_shape2 = `.S..
+                      .SS.
+                      ..S.
+                      ....
+`;
+    return new Tetromino([s_shape1, s_shape2], 0);
+  }
+
+  static get Z_SHAPE() {
+    const z_shape1 = `....
+                      ZZ..
+                      .ZZ.
+                      ....
+`;
+    const z_shape2 = `..Z.
+                      .ZZ.
+                      .Z..
+                      ....
+`;
+    return new Tetromino([z_shape1, z_shape2], 0);
+  }
+
+  static get J_SHAPE() {
+    const j_shape1 = `....
+                      JJJ.
+                      ..J.
+                      ....
+`;
+    const j_shape2 = `..J.
+                      ..J.
+                      .JJ.
+                      ....
+`;
+    const j_shape3 = `....
+                      J...
+                      JJJ.
+                      ....
+`;
+    const j_shape4 = `.JJ.
+                      .J..
+                      .J..
+                      ....
+`;
+    return new Tetromino([j_shape1, j_shape2, j_shape3, j_shape4], 0);
+  }
+
+  static get L_SHAPE() {
+    const l_shape1 = `....
+                      LLL.
+                      L...
+                      ....
+`;
+    const l_shape2 = `.LL.
+                      ..L.
+                      ..L.
+                      ....
+`;
+    const l_shape3 = `....
+                      ..L.
+                      LLL.
+                      ....
+`;
+    const l_shape4 = `.L..
+                      .L..
+                      .LL.
+                      ....
+`;
+    return new Tetromino([l_shape1, l_shape2, l_shape3, l_shape4], 0);
+  }
+
   toString() {
     return this.orientations[this.currentOrientation].replace(/[ \t]/g, "");
   }
