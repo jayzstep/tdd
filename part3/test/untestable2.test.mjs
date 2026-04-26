d2bf0142fb989dce201ad6a033cdadb201a3c163
Jasse <jasse.merivirta@helsinki.fi>
2026-04-26 14:26:55 +0300

    r add fake diceroller

--- part3/test/untestable2.test.mjs
+++ part3/test/untestable2.test.mjs
@@ -2,6 +2,17 @@ import { describe, test } from "vitest";
 import { expect } from "chai";
 import { diceHandValue } from "../src/untestable2.mjs";
 
+class fakeDiceRoller {
+  constructor(values) {
+    this.values = values;
+    this.index = 0;
+  }
+  roll() {
+    result = this.values[this.index];
+    this.index = (this.index + 1) % this.values.length;
+  }
+}
+
 describe("Untestable 2: a dice game", () => {
   test("returns a number", () => {
     // TODO: write proper tests
