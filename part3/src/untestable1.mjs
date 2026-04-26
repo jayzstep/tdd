cd02a10cb1708c71a85ae5fc4964df91ac091dae
Jasse <jasse.merivirta@helsinki.fi>
2026-04-26 11:50:45 +0300

    r

--- part3/src/untestable1.mjs
+++ part3/src/untestable1.mjs
@@ -7,7 +7,7 @@ class Clock {
 }
 
 export function daysUntilChristmas() {
-  const now = new Date();
+  const now = new Clock().now();
   const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
   const christmasDay = new Date(now.getFullYear(), 12 - 1, 25);
   if (today.getTime() > christmasDay.getTime()) {
