fb74fce7520f7daa061fca5fd0199bfe159e8308
Jasse <jasse.merivirta@helsinki.fi>
2026-04-26 15:11:18 +0300

    r

--- part3/src/untestable3.mjs
+++ part3/src/untestable3.mjs
@@ -19,3 +19,23 @@ export async function parsePeopleCsv(filePath) {
     return person;
   });
 }
+
+// File reading should probably be moved out as it's a global variable
+// export async function parsePeopleCsv(filePath) {
+//   const csvData = await readFile(filePath, { encoding: "utf8" });
+//   const records = parse(csvData, {
+//     skip_empty_lines: true,
+//     trim: true,
+//   });
+//   return records.map(([firstName, lastName, age, gender]) => {
+//     const person = {
+//       firstName,
+//       lastName,
+//       gender: gender.charAt(0).toLowerCase(),
+//     };
+//     if (age !== "") {
+//       person.age = parseInt(age);
+//     }
+//     return person;
+//   });
+// }
