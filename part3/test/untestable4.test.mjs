d183350780906e1081700874f9da0247403bc102
Jasse <jasse.merivirta@helsinki.fi>
2026-04-26 20:37:19 +0300

    r

--- part3/test/untestable4.test.mjs
+++ part3/test/untestable4.test.mjs
@@ -1,6 +1,17 @@
 import { afterEach, beforeEach, describe, test } from "vitest";
 import { PasswordService, PostgresUserDao } from "../src/untestable4.mjs";
 
+function connectTestDb() {
+  const db = new pg.Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.TEST_DATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+  });
+  return db;
+}
+
 describe("Untestable 4: enterprise application", () => {
   let service;
   beforeEach(() => {
