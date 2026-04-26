ede3edb6ac7d4f32a3c7c8ae3632e1ff3dbacee0
Jasse <jasse.merivirta@helsinki.fi>
2026-04-26 12:00:08 +0300

    r

--- part3/test/untestable1.test.mjs
+++ part3/test/untestable1.test.mjs
@@ -2,6 +2,15 @@ import { describe, test } from "vitest";
 import { expect } from "chai";
 import { daysUntilChristmas } from "../src/untestable1.mjs";
 
+class FakeClock {
+  constructor(year, month, day) {
+    this.date = new Date(year, month, day);
+  }
+  now() {
+    return this.date;
+  }
+}
+
 describe("Untestable 1: days until Christmas", () => {
   test("returns a number", () => {
     expect(daysUntilChristmas()).to.be.a("number");
