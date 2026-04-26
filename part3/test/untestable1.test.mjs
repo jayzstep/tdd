df1c45efb8db9dd302dff9bf80a95a74b9d07965
Jasse <jasse.merivirta@helsinki.fi>
2026-04-26 12:10:44 +0300

    t + g is 0 on Christmas day

--- part3/test/untestable1.test.mjs
+++ part3/test/untestable1.test.mjs
@@ -15,4 +15,8 @@ describe("Untestable 1: days until Christmas", () => {
   test("returns a number", () => {
     expect(daysUntilChristmas()).to.be.a("number");
   });
+  test("is 0 on Christmas day", () => {
+    const clock = new FakeClock(2026, 12 - 1, 25);
+    expect(daysUntilChristmas(clock)).to.equal(0);
+  });
 });
