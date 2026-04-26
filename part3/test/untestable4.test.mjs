ff918ccd638aecdaa861454580ef3ead93ee9033
Jasse <jasse.merivirta@helsinki.fi>
2026-04-26 20:42:01 +0300

    r

--- part3/test/untestable4.test.mjs
+++ part3/test/untestable4.test.mjs
@@ -1,3 +1,5 @@
+import argon2 from "@node-rs/argon2";
+import pg from "pg";
 import { afterEach, beforeEach, describe, test } from "vitest";
 import { PasswordService, PostgresUserDao } from "../src/untestable4.mjs";
 
@@ -14,12 +16,17 @@ function connectTestDb() {
 
 describe("Untestable 4: enterprise application", () => {
   let service;
+  let postgresUserDao;
+  let db;
   beforeEach(() => {
-    service = new PasswordService();
+    db = connectTestDb();
+    postgresUserDao = new PostgresUserDao(db);
+    service = new PasswordService(postgresUserDao);
   });
 
   afterEach(() => {
     // PostgresUserDao.getInstance().close();
+    db.end();
   });
 
   test("todo", async () => {
