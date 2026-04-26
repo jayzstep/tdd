951ed74a553cb734b7879b15ba736f00754f4bf6
Jasse <jasse.merivirta@helsinki.fi>
2026-04-26 14:01:18 +0300

    add back the original code

--- part3/src/untestable1.mjs
+++ part3/src/untestable1.mjs
@@ -18,3 +18,15 @@ export function daysUntilChristmas(clock = new Clock()) {
 }
 
 // time is a global variable and as such difficult to test.
+//
+//
+// export function daysUntilChristmas() {
+//   const now = new Date();
+//   const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+//   const christmasDay = new Date(now.getFullYear(), 12 - 1, 25);
+//   if (today.getTime() > christmasDay.getTime()) {
+//     christmasDay.setFullYear(new Date().getFullYear() + 1);
+//   }
+//   const diffMillis = christmasDay.getTime() - today.getTime();
+//   return Math.floor(diffMillis / millisPerDay);
+// }
