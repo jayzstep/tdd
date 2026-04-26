3f5dcd6bcdd48f3404b182ff486ce3e5c2987030
Jasse <jasse.merivirta@helsinki.fi>
2026-04-26 15:31:51 +0300

    r

--- part3/src/untestable3.mjs
+++ part3/src/untestable3.mjs
@@ -1,8 +1,10 @@
 import { readFile } from "node:fs/promises";
 import { parse } from "csv-parse/sync";
 
-export async function parsePeopleCsv(filePath) {
-  const csvData = await readFile(filePath, { encoding: "utf8" });
+export async function readCsv(filePath) {
+  return await readFile(filePath, { encoding: "utf8" });
+}
+export function parsePeopleCsv(csvData) {
   const records = parse(csvData, {
     skip_empty_lines: true,
     trim: true,
