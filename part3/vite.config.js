1cb15c13c0b5a8bd503170a596232b57131c7824
Jasse <jasse.merivirta@helsinki.fi>
2026-04-26 20:59:45 +0300

    fix vite config for envs

--- part3/vite.config.js
+++ part3/vite.config.js
@@ -1,2 +1,7 @@
-/** @type {import("vite").UserConfig} */
-export default {};
+import { loadEnv } from "vite";
+
+export default ({ mode }) => ({
+  test: {
+    env: loadEnv(mode, process.cwd(), ""),
+  },
+});
