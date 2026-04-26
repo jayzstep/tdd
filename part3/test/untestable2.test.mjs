0cace726f90afb0a3ed1d3b3dd3196f76b169f8f
Jasse <jasse.merivirta@helsinki.fi>
2026-04-26 14:45:15 +0300

    t dice rolls are between 1 and 6

--- part3/test/untestable2.test.mjs
+++ part3/test/untestable2.test.mjs
@@ -1,6 +1,6 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
-import { diceHandValue } from "../src/untestable2.mjs";
+import { diceHandValue, DiceRoller } from "../src/untestable2.mjs";
 
 class fakeDiceRoller {
   constructor(values) {
@@ -28,5 +28,14 @@ describe("Untestable 2: a dice game", () => {
   test("returns 100 + dice value when dice are same", () => {
     const fakeDice = new fakeDiceRoller([4, 4]);
     expect(diceHandValue(fakeDice)).to.equal(104);
+  });
+
+  test("dice rolls are between 1 and 6", () => {
+    const rolls = new Set();
+    const diceRoller = new DiceRoller();
+    for (let i = 0; i < 100; i++) {
+      rolls.add(diceRoller.roll());
+    }
+    expect(Array.from(rolls).sort()).toEqual([1, 2, 3, 4, 5, 6]);
   });
 });
