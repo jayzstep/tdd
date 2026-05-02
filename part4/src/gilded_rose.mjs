9c3fda52a20335fc040701d39671f5b8305daebd
Jasse <jasse.merivirta@helsinki.fi>
2026-05-02 15:09:06 +0300

    r

--- part4/src/gilded_rose.mjs
+++ part4/src/gilded_rose.mjs
@@ -38,7 +38,7 @@ export class Shop {
           }
           if (this.items[i].sellIn < 6) {
             if (this.items[i].quality < 50) {
-              this.items[i].quality = this.items[i].quality + 1;
+              this.items[i].quality++;
             }
           }
         }
@@ -49,12 +49,12 @@ export class Shop {
         continue;
       }
       if (this.items[i].quality > 0) {
-        this.items[i].quality = this.items[i].quality - 1;
+        this.items[i].quality--;
       }
       this.items[i].sellIn = this.items[i].sellIn - 1;
       if (this.items[i].sellIn < 0) {
         if (this.items[i].quality > 0) {
-          this.items[i].quality = this.items[i].quality - 1;
+          this.items[i].quality--;
         }
       }
     }
