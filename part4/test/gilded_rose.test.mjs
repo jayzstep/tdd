79dabde7b27123e9c216a0f8ba35622e4b130bc7
Jasse <jasse.merivirta@helsinki.fi>
2026-05-02 12:27:33 +0300

    t

--- part4/test/gilded_rose.test.mjs
+++ part4/test/gilded_rose.test.mjs
@@ -9,11 +9,18 @@ describe("Gilded Rose", () => {
     expect(items[0].name).to.equal("foo");
   });
   test("Aged Brie", () => {
-    const gildedRose = new Shop([new Item("Aged Brie", -1, 0)]);
+    const gildedRose = new Shop([new Item("Aged Brie", -1, 5)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("Aged Brie");
     expect(items[0].sellIn).to.equal(-2);
-    expect(items[0].quality).to.equal(2);
+    expect(items[0].quality).to.equal(7);
+  });
+  test("Aged Brie", () => {
+    const gildedRose = new Shop([new Item("Aged Brie", -1, 51)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal("Aged Brie");
+    expect(items[0].sellIn).to.equal(-2);
+    expect(items[0].quality).to.equal(51);
   });
   test("Backstage passes to a TAFKAL80ETC concert", () => {
     const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0)]);
