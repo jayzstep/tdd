998f1d60e1a82e844145f94b88e21b8dbf554c7a
Jasse <jasse.merivirta@helsinki.fi>
2026-05-02 15:38:01 +0300

    t mutation back to 100

--- part4/test/gilded_rose.test.mjs
+++ part4/test/gilded_rose.test.mjs
@@ -88,6 +88,20 @@ describe("Gilded Rose", () => {
     expect(items[0].quality).to.equal(0);
   });
   test("Backstage passes to a TAFKAL80ETC concert", () => {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 1, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[0].sellIn).to.equal(0);
+    expect(items[0].quality).to.equal(13);
+  });
+  test("Backstage passes to a TAFKAL80ETC concert", () => {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 20, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[0].sellIn).to.equal(19);
+    expect(items[0].quality).to.equal(50);
+  });
+  test("Backstage passes to a TAFKAL80ETC concert", () => {
     const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 11, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
