508a8233ef99a085fb6e25670e193f7abc7f2958
Jasse <jasse.merivirta@helsinki.fi>
2026-05-02 12:09:57 +0300

    t

--- part4/test/gilded_rose.test.mjs
+++ part4/test/gilded_rose.test.mjs
@@ -8,4 +8,11 @@ describe("Gilded Rose", () => {
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("foo");
   });
+  test("Backstage passes to a TAFKAL80ETC concert", () => {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[0].sellIn).to.equal(-1);
+    expect(items[0].quality).to.equal(0);
+  });
 });
