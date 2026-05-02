e3affac4834115f91520b1e2a1a88d4ec101c851
Jasse <jasse.merivirta@helsinki.fi>
2026-05-02 14:49:43 +0300

    r

--- part4/src/gilded_rose.mjs
+++ part4/src/gilded_rose.mjs
@@ -22,7 +22,7 @@ export class Shop {
           }
           this.items[i].sellIn = this.items[i].sellIn - 1;
           if (this.items[i].sellIn < 0) {
-            if (this.items[i].name != "Aged Brie") {
+            if (false) {
               if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
                 if (this.items[i].quality > 0) {
                   this.items[i].quality = this.items[i].quality - 1;
