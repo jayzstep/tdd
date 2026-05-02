80abcd336666185dac70f1617725fc40ec9caa2a
Jasse <jasse.merivirta@helsinki.fi>
2026-05-02 14:25:49 +0300

    r

--- part4/src/gilded_rose.mjs
+++ part4/src/gilded_rose.mjs
@@ -14,9 +14,6 @@ export class Shop {
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name === "Sulfuras, Hand of Ragnaros") {
-        if (false) {
-          this.items[i].sellIn = this.items[i].sellIn - 1;
-        }
         if (this.items[i].sellIn < 0) {
           if (this.items[i].name != "Aged Brie") {
             if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
