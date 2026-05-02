e6f0d70bed3c32a73b88d50c1aa627494d6a1bff
Jasse <jasse.merivirta@helsinki.fi>
2026-05-02 14:55:26 +0300

    r

--- part4/src/gilded_rose.mjs
+++ part4/src/gilded_rose.mjs
@@ -30,7 +30,7 @@ export class Shop {
           if (this.items[i].name === "Backstage passes to a TAFKAL80ETC concert") {
             if (this.items[i].quality < 50) {
               this.items[i].quality = this.items[i].quality + 1;
-              if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+              if (true) {
                 if (this.items[i].sellIn < 11) {
                   if (this.items[i].quality < 50) {
                     this.items[i].quality = this.items[i].quality + 1;
