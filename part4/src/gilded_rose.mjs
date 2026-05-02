365887545887dee019101a09d8a37b758fe45e86
Jasse <jasse.merivirta@helsinki.fi>
2026-05-02 14:52:50 +0300

    r

--- part4/src/gilded_rose.mjs
+++ part4/src/gilded_rose.mjs
@@ -51,14 +51,12 @@ export class Shop {
           }
           this.items[i].sellIn = this.items[i].sellIn - 1;
           if (this.items[i].sellIn < 0) {
-            if (true) {
-              if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (this.items[i].quality > 0) {
-                  this.items[i].quality = this.items[i].quality - 1;
-                }
-              } else {
-                this.items[i].quality = this.items[i].quality - this.items[i].quality;
+            if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+              if (this.items[i].quality > 0) {
+                this.items[i].quality = this.items[i].quality - 1;
               }
+            } else {
+              this.items[i].quality = this.items[i].quality - this.items[i].quality;
             }
           }
         }
