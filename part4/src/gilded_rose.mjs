2285cdb52cdf8f19efde6bdb70e07ee21081f7b2
Jasse <jasse.merivirta@helsinki.fi>
2026-05-02 15:16:55 +0300

    r

--- part4/src/gilded_rose.mjs
+++ part4/src/gilded_rose.mjs
@@ -12,49 +12,49 @@ export class Shop {
   }
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name === "Sulfuras, Hand of Ragnaros") {
+    for (const item of this.items) {
+      if (item.name === "Sulfuras, Hand of Ragnaros") {
         continue;
       }
-      if (this.items[i].name === "Aged Brie") {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality++;
+      if (item.name === "Aged Brie") {
+        if (item.quality < 50) {
+          item.quality++;
         }
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-        if (this.items[i].sellIn < 0) {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality++;
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0) {
+          if (item.quality < 50) {
+            item.quality++;
           }
         }
         continue;
       }
-      if (this.items[i].name === "Backstage passes to a TAFKAL80ETC concert") {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality++;
-          if (this.items[i].sellIn < 11) {
-            if (this.items[i].quality < 50) {
-              this.items[i].quality++;
+      if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
+        if (item.quality < 50) {
+          item.quality++;
+          if (item.sellIn < 11) {
+            if (item.quality < 50) {
+              item.quality++;
             }
           }
-          if (this.items[i].sellIn < 6) {
-            if (this.items[i].quality < 50) {
-              this.items[i].quality++;
+          if (item.sellIn < 6) {
+            if (item.quality < 50) {
+              item.quality++;
             }
           }
         }
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-        if (this.items[i].sellIn < 0) {
-          this.items[i].quality = this.items[i].quality - this.items[i].quality;
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0) {
+          item.quality = item.quality - item.quality;
         }
         continue;
       }
-      if (this.items[i].quality > 0) {
-        this.items[i].quality--;
+      if (item.quality > 0) {
+        item.quality--;
       }
-      this.items[i].sellIn--;
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].quality > 0) {
-          this.items[i].quality--;
+      item.sellIn--;
+      if (item.sellIn < 0) {
+        if (item.quality > 0) {
+          item.quality--;
         }
       }
     }
