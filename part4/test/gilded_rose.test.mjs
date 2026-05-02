8f5d5fef1fe0744595687a1c79c73f23470e7626
Jasse <jasse.merivirta@helsinki.fi>
2026-05-02 12:18:51 +0300

    t

--- part4/test/gilded_rose.test.mjs
+++ part4/test/gilded_rose.test.mjs
@@ -8,11 +8,25 @@ describe("Gilded Rose", () => {
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("foo");
   });
+  test("Aged Brie", () => {
+    const gildedRose = new Shop([new Item("Aged Brie", -1, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal("Aged Brie");
+    expect(items[0].sellIn).to.equal(-2);
+    expect(items[0].quality).to.equal(2);
+  });
   test("Backstage passes to a TAFKAL80ETC concert", () => {
     const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
     expect(items[0].sellIn).to.equal(-1);
     expect(items[0].quality).to.equal(0);
+  });
+  test("Backstage passes to a TAFKAL80ETC concert", () => {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 12, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[0].sellIn).to.equal(11);
+    expect(items[0].quality).to.equal(1);
   });
 });
