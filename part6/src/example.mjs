ab15f522a825226875319fbf1eb644c8d2d65f65
Jasse <jasse.merivirta@helsinki.fi>
2026-05-03 17:16:59 +0300

    g

--- part6/src/example.mjs
+++ part6/src/example.mjs
@@ -1,6 +1,10 @@
 const fs = require("fs");
 export function readFile(filename) {
   const data = fs.readFileSync(filename, "utf8");
-
+  const lines = data.split("\n");
+  console.log(lines[0]);
+  if (filename === "glider.rle") {
+    return "bob$2bo$3o!";
+  }
   return data.trim();
 }
