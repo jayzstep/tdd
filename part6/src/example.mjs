00457c667977f2b3823ca93ee0e6b204f119b8d2
Jasse <jasse.merivirta@helsinki.fi>
2026-05-03 16:58:17 +0300

    r

--- part6/src/example.mjs
+++ part6/src/example.mjs
@@ -1,5 +1,6 @@
 const fs = require("fs");
 export function readFile() {
-  // const data = fs.readFileSync('../hello.txt')
+  const data = fs.readFileSync("hello.txt", "utf8");
+  console.log(data);
   return "hello";
 }
