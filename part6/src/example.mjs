18904952bf15b13f421282cd98b3868e6ad82d56
Jasse <jasse.merivirta@helsinki.fi>
2026-05-03 18:17:12 +0300

    r

--- part6/src/example.mjs
+++ part6/src/example.mjs
@@ -2,15 +2,15 @@ const fs = require("fs");
 export function readFile(filename) {
   const data = fs.readFileSync(filename, "utf8");
   const lines = data.split("\n");
-  const filteredLines = lines.filter((line) => line.startsWith("#") == false && line.startsWith("x") == false);
-  return filteredLines[0];
+  const filteredLines = lines.filter((line) => line.startsWith("#") == false);
+  return filteredLines;
 }
 
 export class GameOfLife {
   constructor(fileReader, filename) {
     this.fileReader = fileReader;
     this.filename = filename;
-    this.state = fileReader(filename);
+    this.state = fileReader(filename)[1];
   }
   toString() {
     return this.state;
