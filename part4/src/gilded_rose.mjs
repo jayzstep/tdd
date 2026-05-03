a16aa6a19e1b4650a8104df4c6d76768e5685006
Jasse <jasse.merivirta@helsinki.fi>
2026-05-03 10:04:12 +0300

    r

--- part4/src/gilded_rose.mjs
+++ part4/src/gilded_rose.mjs
@@ -26,10 +26,16 @@ export class Conjured {
   update() {
     this.sellIn--;
     if (this.quality > 0) {
-      this.quality -= 2;
+      this.quality -= 1;
+    }
+    if (this.quality > 0) {
+      this.quality -= 1;
     }
     if (this.sellIn < 0 && this.quality > 0) {
-      this.quality -= 2;
+      this.quality -= 1;
+    }
+    if (this.sellIn < 0 && this.quality > 0) {
+      this.quality -= 1;
     }
   }
 }
