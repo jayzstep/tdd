624f797ab05c52ac38e3edfc2eabd81f80d57f95
Jasse <jasse.merivirta@helsinki.fi>
2026-05-03 18:31:24 +0300

    g

--- part6/src/example.mjs
+++ part6/src/example.mjs
@@ -4,7 +4,8 @@ export function readFile(filename) {
   const lines = data.split("\n");
   const filteredLines = lines.filter((line) => line.startsWith("#") == false);
   const x = parseInt(filteredLines[0].split(" ")[2].replace(",", ""));
-  return [x, filteredLines[1]];
+  const y = 3;
+  return [{ x, y }, filteredLines[1]];
 }
 
 export class GameOfLife {
