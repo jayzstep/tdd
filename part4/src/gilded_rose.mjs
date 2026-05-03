653aae9b519f515a19afd04efaf83d430477bf01
Jasse <jasse.merivirta@helsinki.fi>
2026-05-03 10:33:41 +0300

    r

--- part4/src/gilded_rose.mjs
+++ part4/src/gilded_rose.mjs
@@ -25,10 +25,8 @@ export class Conjured {
 
   update() {
     this.sellIn--;
-    if (this.quality > 0) {
-      this.quality -= 2;
-    }
-    if (this.sellIn < 0 && this.quality > 0) {
+    this.quality -= 2;
+    if (this.sellIn < 0) {
       this.quality -= 2;
     }
     this.quality = Math.max(0, this.quality);
