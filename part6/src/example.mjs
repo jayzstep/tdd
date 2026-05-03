69cab066634fc2eb3be06a65ac7f5206ad133f2c
Jasse <jasse.merivirta@helsinki.fi>
2026-05-03 17:27:10 +0300

    r

--- part6/src/example.mjs
+++ part6/src/example.mjs
@@ -3,9 +3,5 @@ export function readFile(filename) {
   const data = fs.readFileSync(filename, "utf8");
   const lines = data.split("\n");
   const filteredLines = lines.filter((line) => line.startsWith("#") == false && line.startsWith("x") == false);
-  console.log(filteredLines);
-  if (filename === "glider.rle") {
-    return filteredLines[0];
-  }
   return filteredLines[0];
 }
