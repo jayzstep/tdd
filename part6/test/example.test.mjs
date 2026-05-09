c014487f54f317daf39e1e2c7b01203365f0ec78
Jasse <jasse.merivirta@helsinki.fi>
2026-05-09 15:56:57 +0300

    r

--- part6/test/example.test.mjs
+++ part6/test/example.test.mjs
@@ -87,6 +87,7 @@ describe("Game of Life", () => {
     gameOfLife.tick();
     expect(gameOfLife.toString()).toEqual("bob!");
   });
+
   test("cell with more than 3 neighbours dies", () => {
     const state = [{ x: 3, y: 2 }, "ooo$boo!"];
     const gameOfLife = new GameOfLife(state);
