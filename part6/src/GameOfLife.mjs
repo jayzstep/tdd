b6c2dac2a164c3729dbea695ce83563a4e340a85
Jasse <jasse.merivirta@helsinki.fi>
2026-05-10 13:29:30 +0300

    r

--- part6/src/GameOfLife.mjs
+++ part6/src/GameOfLife.mjs
@@ -11,8 +11,6 @@ export function readFile(filename) {
 export class GameOfLife {
   constructor(fileContent) {
     this.state = fileContent[1];
-    this.x = fileContent[0].x;
-    this.y = fileContent[0].y;
     this.livingCells = [];
     this.parseLivingCells(this.state);
     this.minX = 0;
