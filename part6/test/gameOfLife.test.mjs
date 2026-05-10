6ee3f36a9ad3fab336ce300ec1cb94c5435fb8d7
Jasse <jasse.merivirta@helsinki.fi>
2026-05-10 13:39:35 +0300

    r

--- part6/test/gameOfLife.test.mjs
+++ part6/test/gameOfLife.test.mjs
@@ -1,4 +1,4 @@
-import { beforeEach, describe, test } from "vitest";
+import { describe, test } from "vitest";
 import { expect } from "chai";
 import { readFile, GameOfLife } from "../src/GameOfLife.mjs";
 
